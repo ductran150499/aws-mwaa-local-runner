1334c674654002419aa2382b1684772447610b92
Hieu Pham <hieu.pham@employmenthero.com>
2024-10-29 02:40:08 +0700

    Combine script

--- backups/code/crawl_local.py
+++ backups/code/crawl_local.py
@@ -11,6 +11,8 @@ import csv
 import json
 import re
 
+# Setup WebDriver (make sure to specify the path to your ChromeDriver)
+# setUpWebDriver
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1200x10000')
@@ -18,8 +20,10 @@ options.add_argument('window-size=1200x10000')
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+wait = WebDriverWait(driver, 15)
+
 # Hàm lấy thông tin về địa điểm (location_info)
-def get_location_info(driver, wait):
+def get_location_info():
     location_info = {}
     try:
         location_info['title'] = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.DUwDvf'))).text.strip()
@@ -40,7 +44,7 @@ def extract_lat_long(url):
         return None
 
 # Hàm lấy thông tin giờ mở cửa
-def get_opening_hours(driver, wait):
+def get_opening_hours():
     try:
         expand_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span[aria-label="Show open hours for the week"]')))
         expand_button.click()
@@ -71,7 +75,7 @@ def get_opening_hours(driver, wait):
         return None
 
 # Hàm lấy thông tin giá tiền
-def get_price_info(driver, wait):
+def get_price_info(wait):
     try:
         expand_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.IgTTAf')))
         expand_button.click()
@@ -101,7 +105,7 @@ def get_price_info(driver, wait):
         return None
 
 # Hàm lấy số liệu thống kê đánh giá
-def get_review_stats(driver, wait):
+def get_review_stats():
     try:
         rate = driver.find_element(By.CSS_SELECTOR, 'div.jANrlb div.fontDisplayLarge').text.strip()
 
@@ -131,7 +135,7 @@ def get_review_stats(driver, wait):
         return None
 
 # Hàm để chuyển qua tab "About" và thu thập thông tin bổ sung
-def get_additional_info(driver, wait):
+def get_additional_info():
     try:
         about_tab = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-tab-index="2"]')))
         click_with_js(driver, about_tab)
@@ -186,7 +190,7 @@ def get_ratings(review):
 
     return food_rating, service_rating, atmosphere_rating
 
-def get_user_reviews(driver, wait, sort_type):
+def get_user_reviews(sort_type):
     user_reviews = []
 
     try:
@@ -255,9 +259,7 @@ def get_user_reviews(driver, wait, sort_type):
 def get_reviews_data(url):   
     driver.get(url)
 
-    wait = WebDriverWait(driver, 15)
-
-    location_info = get_location_info(driver, wait)
+    location_info = get_location_info()
 
     # Tìm vĩ độ và kinh độ trong URL (thường nằm sau "@")
     time.sleep(3)
@@ -266,61 +268,155 @@ def get_reviews_data(url):
     if lat_long:
         location_info['lat_long'] = lat_long
 
-    opening_hours = get_opening_hours(driver, wait)
+    opening_hours = get_opening_hours()
     if opening_hours:
         location_info['opening_hours'] = opening_hours
     
     # Lấy thông tin giá tiền
-    price_info = get_price_info(driver, wait)
+    price_info = get_price_info()
     if price_info:
         location_info['price_info'] = price_info
 
     # Lấy thông tin thống kê reviews
-    review_stats = get_review_stats(driver, wait)
+    review_stats = get_review_stats()
     if review_stats:
         location_info['review_stats'] = review_stats
     # ========
 
     # Lấy thông tin bổ sung từ tab "About"
-    additional_info = get_additional_info(driver, wait)
+    additional_info = get_additional_info()
     if additional_info:
         location_info['additional_info'] = additional_info
     # ========
     
     # Lấy thông tin danh sách review
-    most_relevant_reviews = get_user_reviews(driver, wait, sort_type=0)
+    most_relevant_reviews = get_user_reviews(sort_type=0)
     if most_relevant_reviews:
         location_info['most_relevant_reviews'] = most_relevant_reviews
 
-    newest_reviews = get_user_reviews(driver, wait, sort_type=1)
+    newest_reviews = get_user_reviews(sort_type=1)
     if newest_reviews:
         location_info['newest_reviews'] = newest_reviews
 
-    highest_rating_reviews = get_user_reviews(driver, wait, sort_type=2)
+    highest_rating_reviews = get_user_reviews(sort_type=2)
     if highest_rating_reviews:
         location_info['highest_rating_reviews'] = highest_rating_reviews
 
-    lowest_rating_reviews = get_user_reviews(driver, wait, sort_type=3)
+    lowest_rating_reviews = get_user_reviews(sort_type=3)
     if lowest_rating_reviews:
         location_info['lowest_rating_reviews'] = lowest_rating_reviews
     
     return location_info
 
-# Load csv from phase 1
-with open('google_places_data_by_category_phase_1.csv', 'r') as f:
-    reader = csv.reader(f)
-    data = list(reader)
-    result = []
-    # call get_location_info for each row
-    for row in data:
-        category, location, keyword, name, place_url = row
-        print(f"Scraping {name}")
-        location_info = get_reviews_data(place_url)
-        result.append(location_info)
-    # write to json
-    with open('google_places_data_by_category_phase_2.json', 'w') as f:
-        json.dump(result, f, indent=4)
+def scrape_google_maps(keyword, location, category, results_limit=50):
+    keyword = f"{category} ở {location}"
+    driver.get(f"https://www.google.com/maps/search/{keyword}")
     
+    places_data = []
+    
+    wait = WebDriverWait(driver, 20)    
+    wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[starts-with(@href, 'https://www.google.com/maps/place/')]")))
+        
+    place_cards = driver.find_elements(By.XPATH, "//a[starts-with(@href, 'https://www.google.com/maps/place/')]")[:results_limit]
+    
+    for place in place_cards:
+        try:
+            place_url = place.get_attribute("href")
+            name = place.get_attribute("aria-label")
+            location_info = get_reviews_data(place_url)
+            places_data.append({
+                "category": category,
+                "location": location,
+                "keyword": keyword,
+                "name": name,
+                "url": place_url,
+                "location_info": location_info
+            })            
+        except Exception as e:
+            print(f"Error scraping place: {e}")
+    
+    return places_data
 
+locations = [
+    "Quận 1, TP. Hồ Chí Minh",
+    "Quận 3, TP. Hồ Chí Minh",
+    "Quận 4, TP. Hồ Chí Minh",
+    "Quận 5, TP. Hồ Chí Minh",
+    "Quận 6, TP. Hồ Chí Minh",
+    "Quận 7, TP. Hồ Chí Minh",
+    "Quận 8, TP. Hồ Chí Minh",
+    "Quận 10, TP. Hồ Chí Minh",
+    "Quận 11, TP. Hồ Chí Minh",
+    "Quận 12, TP. Hồ Chí Minh",
+    
+    "TP. Thủ Đức, TP. Hồ Chí Minh",
+    
+    "Quận Bình Thạnh, TP. Hồ Chí Minh",
+    "Quận Phú Nhuận, TP. Hồ Chí Minh",
+    "Quận Tân Bình, TP. Hồ Chí Minh",
+    "Quận Tân Phú, TP. Hồ Chí Minh",
+    "Quận Gò Vấp, TP. Hồ Chí Minh",
+    "Quận Bình Tân, TP. Hồ Chí Minh",
+    
+    "Huyện Bình Chánh, TP. Hồ Chí Minh",
+    "Huyện Củ Chi, TP. Hồ Chí Minh",
+    "Huyện Hóc Môn, TP. Hồ Chí Minh",
+    "Huyện Nhà Bè, TP. Hồ Chí Minh",
+    "Huyện Cần Giờ, TP. Hồ Chí Minh",
+]
+categories = [
+    "nhà hàng",
+    "món Việt",
+    "món Hàn",
+    "món Nhật",
+    "món Trung",
+    "món Thái",
+    "món Âu",
+    "món Ấn",
+    "món chay",
+    "cà phê",
+    "phở",
+    "bánh mì",
+    "bún riêu",
+    "bún bò",
+    "bún chả",
+    "bún đậu mắm tôm",
+    "bún mắm",
+    "bún thịt nướng",
+    "quán bia",
+    "buffet",
+    "quán ăn",
+    "quán nhậu",
+    "quán nướng",
+    "quán lẩu",
+    "quán cơm",
+    "quán chay",
+    "quán ăn vặt",
+    "quán trà sữa",
+    "quán kem",
+    "quán ốc",
+    "quán chè",
+] 
+
+# Loop through keywords and scrape data
+all_data = []
+
+for i in range(len(locations)):
+        location = locations[i]
+        for j in range(len(categories)):
+            category = categories[j]
+            progress = (i * len(categories) + j + 1) / (len(locations) * len(categories))
+            print(f"[{progress:.2%}]: Scraping {category} ở {location}")
+            keyword = f"{category} ở {location}"
+            places_data = scrape_google_maps(keyword, location, category)
+            print(f"Found {len(places_data)} places")
+            all_data.extend(places_data)
+        # write to json file
+        with open(f"ggmaps_data_{location}.json", "w") as f:
+            json.dump(all_data, f, indent=4)
+
+with open(f"ggmaps_data_final.json", "w") as f:
+    json.dump(all_data, f, indent=4)
+        
 driver.quit()
 
