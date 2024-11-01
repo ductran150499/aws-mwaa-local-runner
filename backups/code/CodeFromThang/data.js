3a88ba3894526ca238b355bb51087538db2dc1d3
VXDThang <73100267+VXDThang@users.noreply.github.com>
2024-11-02 05:37:55 +0700

    Sample categories and locations

---
+++ backups/code/CodeFromThang/data.js
@@ -0,0 +1,263 @@
+data = {
+    "locations": [{
+            "keyToSearch": "Quận 1, Hồ Chí Minh",
+            "componentFileName": "Quan1_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 2, Hồ Chí Minh",
+            "componentFileName": "Quan2_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 3, Hồ Chí Minh",
+            "componentFileName": "Quan3_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 4, Hồ Chí Minh",
+            "componentFileName": "Quan4_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 5, Hồ Chí Minh",
+            "componentFileName": "Quan5_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 6, Hồ Chí Minh",
+            "componentFileName": "Quan6_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 7, Hồ Chí Minh",
+            "componentFileName": "Quan7_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 8, Hồ Chí Minh",
+            "componentFileName": "Quan8_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 9, Hồ Chí Minh",
+            "componentFileName": "Quan9_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 10, Hồ Chí Minh",
+            "componentFileName": "Quan10_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 11, Hồ Chí Minh",
+            "componentFileName": "Quan11_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Quận 12, Hồ Chí Minh",
+            "componentFileName": "Quan12_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Thủ Đức, Hồ Chí Minh",
+            "componentFileName": "ThuDuc_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Bình Thạnh, Hồ Chí Minh",
+            "componentFileName": "BinhThanh_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Phú Nhuận, Hồ Chí Minh",
+            "componentFileName": "PhuNhuan_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Tân Bình, Hồ Chí Minh",
+            "componentFileName": "TanBinh_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Tân Phú, Hồ Chí Minh",
+            "componentFileName": "TanPhu_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Gò Vấp, Hồ Chí Minh",
+            "componentFileName": "GoVap_HoChiMinh"
+        },
+        {
+            "keyToSearch": "Bình Tân, Hồ Chí Minh",
+            "componentFileName": "BinhTan_HoChiMinh"
+        }
+    ],
+    "categories": [{
+            "keyToSearch": "món ăn Việt Nam",
+            "componentFileName": "MonAnVietNam",
+            "maxDataCollect": 30
+        },
+        {
+            "keyToSearch": "món ăn Hàn Quốc",
+            "componentFileName": "MonAnHanQuoc",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "món ăn Nhật Bản",
+            "componentFileName": "MonAnNhatBan",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "món ăn Trung Quốc",
+            "componentFileName": "MonAnTrungQuoc",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "món ăn Thái Lan",
+            "componentFileName": "MonAnThaiLan",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "món ăn Ấn Độ",
+            "componentFileName": "MonAnAnDo",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "món chay",
+            "componentFileName": "MonChay",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "quán cà phê",
+            "componentFileName": "QuanCaPhe",
+            "maxDataCollect": 50
+        },
+        {
+            "keyToSearch": "phở",
+            "componentFileName": "Pho",
+            "maxDataCollect": 50
+        },
+        {
+            "keyToSearch": "bún bò",
+            "componentFileName": "BunBo",
+            "maxDataCollect": 50
+        },
+        {
+            "keyToSearch": "bánh mì",
+            "componentFileName": "BanhMi",
+            "maxDataCollect": 50
+        },
+        {
+            "keyToSearch": "bánh xèo",
+            "componentFileName": "BanhXeo",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "bún chả Hà Nội",
+            "componentFileName": "BunChaHaNoi",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "bún thịt nướng",
+            "componentFileName": "BunThitNuong",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "bún riêu",
+            "componentFileName": "BunRieu",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "hủ tiếu",
+            "componentFileName": "HuTieu",
+            "maxDataCollect": 40
+        },
+        {
+            "keyToSearch": "mì Quảng",
+            "componentFileName": "MiQuang",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "cơm tấm",
+            "componentFileName": "ComTam",
+            "maxDataCollect": 50
+        },
+        {
+            "keyToSearch": "gỏi cuốn",
+            "componentFileName": "GoiCuon",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "nem nướng",
+            "componentFileName": "NemNuong",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "xôi",
+            "componentFileName": "Xoi",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "bánh cuốn",
+            "componentFileName": "BanhCuon",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "bánh canh",
+            "componentFileName": "BanhCanh",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "gà nướng muối ớt",
+            "componentFileName": "GaNuongMuoiOt",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "lẩu bò",
+            "componentFileName": "LauBo",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "lẩu gà",
+            "componentFileName": "LauGa",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "lẩu cá",
+            "componentFileName": "LauCa",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "lẩu mắm",
+            "componentFileName": "LauMam",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "bò kho",
+            "componentFileName": "BoKho",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "món gà",
+            "componentFileName": "MonGa",
+            "maxDataCollect": 30
+        },
+        {
+            "keyToSearch": "chè",
+            "componentFileName": "Che",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "bún đậu mắm tôm",
+            "componentFileName": "BunDauMamTom",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "quán bia",
+            "componentFileName": "QuanBia",
+            "maxDataCollect": 30
+        },
+        {
+            "keyToSearch": "buffet",
+            "componentFileName": "Buffet",
+            "maxDataCollect": 10
+        },
+        {
+            "keyToSearch": "quán nướng",
+            "componentFileName": "QuanNuong",
+            "maxDataCollect": 20
+        },
+        {
+            "keyToSearch": "ốc",
+            "componentFileName": "Oc",
+            "maxDataCollect": 20
+        }
+    ]
+}
+console.log("Số địa phương: ", data.locations.length)
+console.log("Số món ăn: ", data.categories.length)
+console.log("Số địa điểm sẽ lấy tối đa của mỗi địa phương: ", data.categories.reduce((sum, item) => sum + item.maxDataCollect, 0))
